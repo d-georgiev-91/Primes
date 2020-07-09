66a8b4f544d39ad2a132ab46b337759710aeaaad
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-09 14:12:23 +0300

    Added validators for requests

---
+++ src/Primes.Web/Validators/NextPrimeRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Primes.Web.Models;
+
+namespace Primes.Web.Validators
+{
+    public class NextPrimeRequestValidator : AbstractValidator<NextPrimeRequest>
+    {
+        public NextPrimeRequestValidator()
+        {
+            RuleFor(nextPrimeRequest => nextPrimeRequest.Number)
+                .GreaterThan(0ul);
+        }
+    }
+}
