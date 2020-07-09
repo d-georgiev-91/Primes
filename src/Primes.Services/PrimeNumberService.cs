f329ba7cc608f80f0030c7f6bfc889ef03af7217
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-09 14:31:25 +0300

    Added implementation of IsPrimeNumber

--- src/Primes.Services/PrimeNumberService.cs
+++ src/Primes.Services/PrimeNumberService.cs
@@ -1,8 +1,33 @@
-﻿namespace Primes.Services
+﻿using System;
+
+namespace Primes.Services
 {
     public class PrimeNumberService : IPrimeNumberService
     {
-        public bool IsPrimeNumber(ulong number) => throw new System.NotImplementedException();
+        public bool IsPrimeNumber(ulong number)
+        {
+            if (number <= 3)
+            {
+                return number > 1;
+            }
+
+            if (number % 2 == 0 || number % 3 == 0)
+            {
+                return false;
+            }
+
+            var limit = (ulong)Math.Floor(Math.Sqrt(number));
+
+            for (ulong i = 5; i <= limit; i += 6)
+            {
+                if (number % i == 0 || number % (i + 2) == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
 
         public ulong NextPrimeNumber(ulong number) => throw new System.NotImplementedException();
     }
