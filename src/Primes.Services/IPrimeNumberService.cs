f45dd05c139297881ff96b57727902689204d7ff
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-09 14:16:10 +0300

    Added services

---
+++ src/Primes.Services/IPrimeNumberService.cs
@@ -0,0 +1,9 @@
+﻿namespace Primes.Services
+{
+    public interface IPrimeNumberService
+    {
+        public bool IsPrimeNumber(ulong number);
+
+        public ulong NextPrimeNumber(ulong number);
+    }
+}
---
+++ src/Primes.Services/PrimeNumberService.cs
@@ -0,0 +1,9 @@
+﻿namespace Primes.Services
+{
+    public class PrimeNumberService : IPrimeNumberService
+    {
+        public bool IsPrimeNumber(ulong number) => throw new System.NotImplementedException();
+
+        public ulong NextPrimeNumber(ulong number) => throw new System.NotImplementedException();
+    }
+}
