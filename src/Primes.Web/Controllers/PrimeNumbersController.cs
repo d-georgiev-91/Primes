badb2df1c284c8d8169600e47564e7402de676fb
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-09 14:34:04 +0300

    Added dependency for IPrimeNumberService

---
+++ src/Primes.Web/Controllers/PrimeNumbersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Primes.Services;
+using Primes.Web.Models;
+
+namespace Primes.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PrimeNumbersController : ControllerBase
+    {
+        private readonly IPrimeNumberService _primeNumberService;
+
+        public PrimeNumbersController(IPrimeNumberService primeNumberService) => _primeNumberService = primeNumberService;
+
+        [HttpPost("[action]")]
+        public IActionResult IsPrime(IsPrimeRequest isPrimeRequest) =>
+            Ok(_primeNumberService.IsPrimeNumber(isPrimeRequest.Number));
+
+        [HttpPost("[action]")]
+        public IActionResult NextPrime(NextPrimeRequest nextPrimeRequest) => Ok(_primeNumberService.NextPrimeNumber(nextPrimeRequest.Number));
+    }
+}
