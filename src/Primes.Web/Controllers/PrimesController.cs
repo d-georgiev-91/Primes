9d2543a88a6ad1f8b05ff5f7b8e6fb410fef3166
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-09 13:55:51 +0300

    Added action methods for verifying if number is prime and finding next number

---
+++ src/Primes.Web/Controllers/PrimesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Primes.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GatewayController : ControllerBase
+    {
+        [HttpPost("[action]/{number}")]
+        public IActionResult IsPrime(ulong number) => throw new NotImplementedException();
+
+        [HttpPost("[action]/{number}")]
+        public IActionResult NextPrime(ulong number) => throw new NotImplementedException();
+    }
+}
